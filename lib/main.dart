1bf2f9ffe6367d672687ec5a69a7d39596c2af79
Rémi <remigalichon@gmail.com>
2020-01-06 18:32:37 +0100

    ajout firebase_crashlytics

--- lib/main.dart
+++ lib/main.dart
@@ -5,47 +5,57 @@ import 'package:programme_tv_multisport/src/bloc/bloc.dart';
 import 'package:programme_tv_multisport/src/dialogs/dialogs.dart';
 import 'package:programme_tv_multisport/src/widgets/widgets.dart';
 import 'package:intl/date_symbol_data_local.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
+import 'dart:async';
 
 void main() {
-  initializeDateFormatting("fr_FR", null).then(
-    (_) => runApp(
-      MultiBlocProvider(
-        providers: [
-          BlocProvider<SportBloc>(
-            create: (BuildContext context) => SportBloc(),
-          ),
-          BlocProvider<SportSelectedBloc>(
-            create: (BuildContext context) => SportSelectedBloc(sportListBloc: BlocProvider.of<SportBloc>(context)),
-          ),
-          BlocProvider<DateListBloc>(
-            create: (BuildContext context) => DateListBloc(),
-          ),
-          BlocProvider<DateSelectedBloc>(
-            create: (BuildContext context) => DateSelectedBloc(),
-          ),
-          BlocProvider<ChannelBloc>(
-            create: (BuildContext context) => ChannelBloc(),
-          ),
-          BlocProvider<BroadcastSelectedBloc>(
-            create: (BuildContext context) => BroadcastSelectedBloc(),
-          ),
-          BlocProvider<EventListBloc>(
-            create: (BuildContext context) => EventListBloc(
-                dateSelectedBloc: BlocProvider.of<DateSelectedBloc>(context),
-                sportListBloc: BlocProvider.of<SportBloc>(context),
-                channelListBloc: BlocProvider.of<ChannelBloc>(context),
-                sportSelectedBloc: BlocProvider.of<SportSelectedBloc>(context),
-                broadcastSelectedBloc: BlocProvider.of<BroadcastSelectedBloc>(context)),
-          ),
-          BlocProvider<LoadingBloc>(
-            create: (BuildContext context) => LoadingBloc(
-                eventListBloc: BlocProvider.of<EventListBloc>(context)),
-          )
-        ],
-        child: App(),
+  Crashlytics.instance.enableInDevMode = true;
+  FlutterError.onError = Crashlytics.instance.recordFlutterError;
+
+  runZoned(() {
+    initializeDateFormatting("fr_FR", null).then(
+      (_) => runApp(
+        MultiBlocProvider(
+          providers: [
+            BlocProvider<SportBloc>(
+              create: (BuildContext context) => SportBloc(),
+            ),
+            BlocProvider<SportSelectedBloc>(
+              create: (BuildContext context) => SportSelectedBloc(
+                  sportListBloc: BlocProvider.of<SportBloc>(context)),
+            ),
+            BlocProvider<DateListBloc>(
+              create: (BuildContext context) => DateListBloc(),
+            ),
+            BlocProvider<DateSelectedBloc>(
+              create: (BuildContext context) => DateSelectedBloc(),
+            ),
+            BlocProvider<ChannelBloc>(
+              create: (BuildContext context) => ChannelBloc(),
+            ),
+            BlocProvider<BroadcastSelectedBloc>(
+              create: (BuildContext context) => BroadcastSelectedBloc(),
+            ),
+            BlocProvider<EventListBloc>(
+              create: (BuildContext context) => EventListBloc(
+                  dateSelectedBloc: BlocProvider.of<DateSelectedBloc>(context),
+                  sportListBloc: BlocProvider.of<SportBloc>(context),
+                  channelListBloc: BlocProvider.of<ChannelBloc>(context),
+                  sportSelectedBloc:
+                      BlocProvider.of<SportSelectedBloc>(context),
+                  broadcastSelectedBloc:
+                      BlocProvider.of<BroadcastSelectedBloc>(context)),
+            ),
+            BlocProvider<LoadingBloc>(
+              create: (BuildContext context) => LoadingBloc(
+                  eventListBloc: BlocProvider.of<EventListBloc>(context)),
+            )
+          ],
+          child: App(),
+        ),
       ),
-    ),
-  );
+    );
+  }, onError: Crashlytics.instance.recordError);
 }
 
 class App extends StatelessWidget {
